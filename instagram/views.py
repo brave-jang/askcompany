53a836e63bb71b032a02da4dd32495f9211dfef0
bravejang <wkdrmfmt@gmail.com>
2021-02-17 21:38:36 +0900

    3-4) 클래스 기반 뷰 시작하기

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,7 @@
-from django.core.checks import messages
-from django.shortcuts import render
+from django.db import models
+from django.http import HttpResponse, HttpRequest, Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -12,3 +14,16 @@ def post_list(request):
         'post_list':qs,
         'q' : q,
     })
+
+
+# def post_detail(request, pk):
+#     # try:
+#     #     post = Post.objects.get(pk=pk)
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, "instagram/post_detail.html", {
+#         'post':post
+#     })
+
+post_detail = DetailView.as_view(model=Post)
